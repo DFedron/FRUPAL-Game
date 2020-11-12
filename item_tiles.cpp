b3ff8315f27ee2ea69c285f9fcf4d1722b6306fa
Axxon007 <austintanderson007@gmail.com>
2020-11-12 15:14:56 -0800

    Added the rest of the items to the header file, paritially to the .cpp file

--- item_tiles.cpp
+++ item_tiles.cpp
@@ -1,1 +1,26 @@
+/*
+ File name: item_tiles.cpp
+ Author: Austin Anderson
+ Group #7
+ Version: 0.1
+ */
 #include "item_tiles.h"
+
+//Item base class
+void item::display_info(){
+	//Stub
+	return;
+}
+//Clue class
+
+//Tool class
+
+//Food class
+
+//Obstacle Class
+
+//Trasure Chest Class
+
+//Ship Class
+
+//Binocular Class
--- item_tiles.h
+++ item_tiles.h
@@ -1,3 +1,9 @@
+/*
+ File name: item_tiles.h
+ Author: Austin Anderson
+ Group #7
+ Version: 0.1
+ */
 #ifndef
 #define
 
@@ -6,12 +12,63 @@
 //Abstract Item base class
 class item{
 	public:
-		display_info() = 0;
-		remove() = 0;
+		void display_info();
+		virtual void remove() = 0;
 }
 //Derived clue class
 class clue : public item{
-
+	public:
+	private:
+		
+}
+//Derived tool class
+class tool : public item{
+	public:
+		//Derived from base class
+		void display_info();
+		int remove_whiffles(int w_rmv);
+	private:
+		int tool_cost;
+}
+//Derived food class
+class food : public item{
+	public:
+		void display_info();
+		int add_energy(int e_added);
+		int remove_whiffles(int w_rmv);
+	private:
+		int food_cost;
+		int energy_value;
+}
+//Derived obstacle class
+class obstacle : public item{
+	public:
+		void display_info();
+		int remove_energy(int e_rmv);
+	private:
+		int e_cost;
+}
+//Derived treasure chest class
+/class treasure_chest : public item{
+	public:
+		void display_info();
+		int add_whiffles(int w_added);
+	private:
+		int w_value;
+}
+//Derived ship class
+class ship : public item{
+	public:
+		void display_item();
+		int remove_whiffles(int w_rmv);
+	private:
+		int w_cost;
+}
+//Derived binocular class
+class binoculars : public item{
+	public:
+		void display_info();
+		int add_vision();
 }
 
 #endif
