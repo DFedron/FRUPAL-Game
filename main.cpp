cced5924c0bdbcb4919945c8add059dea5a2b7d2
DFedron <72382514+DFedron@users.noreply.github.com>
2020-11-20 00:38:50 -0800

    Add the start menu
    I move the primary control into the start() function.

--- main.cpp
+++ main.cpp
@@ -8,29 +8,111 @@
 #include <fstream>
 #include <string>
 #include <ncurses.h>
-
+#include<cstdlib>
 #define MENUWIDTH 35 // sets the menu width
 
 using namespace std;
 
+void start();
 int main(int argc, char ** argv) 
 {
   // TODO check for argc#, exit exception for > 2
-  // create hero with input file name for == 2
-  // regular if == 1
-  Hero * hero; // hero object
-  WINDOW * viewport, * gamemenu; // our 2 windows
-  int ch; // input char for getch()
+  if(argc > 1){
+  	cout << "please enter ./frupal\n";
+  	return 0;  
+  }
+  
   
   initscr();
   // TODO check for screen size, error msg/exit out if
   // LINES < 24 or COLS < 80.
+  if(LINES < 24 || COLS < 80){
+  	cout << "Your screen is less than 80*24\n";
+	return 0;
+  }
 
   noecho();                 // doesn't print input char to screen
   keypad(stdscr, true);     // takes in the up/down/left/right
   nodelay(stdscr, true);    // immediately takes in chars from input
   curs_set(0);              // doesn't print cursor to screen
 
+  start_color(); // colors are matched to enum #'s
+  init_pair(1,COLOR_BLACK,COLOR_GREEN);   // MEADOW = 1
+  init_pair(2,COLOR_BLACK,COLOR_MAGENTA); // SWAMP = 2
+  init_pair(3,COLOR_BLACK,COLOR_BLUE);    // WATER = 3
+  init_pair(4,COLOR_BLACK,COLOR_WHITE);   // WALLS = 4
+  init_pair(5,COLOR_BLACK,COLOR_BLACK);   // UNSEEN = 5
+  init_pair(6,COLOR_YELLOW,COLOR_RED);    // Hero
+  init_pair(7,COLOR_WHITE,COLOR_CYAN);    // Royal Diamond
+
+  //////////////////////////////////////////////////////////////////////////////
+  // The primary control of game is move to the function below called start() //
+  //////////////////////////////////////////////////////////////////////////////
+  
+  
+  
+  //This is start menu
+  //Maybe add customized function?
+	string option[2] = {"1.Start Game","0.Exit"};
+	int hightlight = 0;
+  	int choice;//get the input
+  while (true)
+  {	
+  	WINDOW * mainMenu = newwin(7, COLS-16, LINES-13, 7);
+	box(mainMenu,0,0);
+	refresh();
+	wrefresh(mainMenu);
+	keypad(mainMenu, true);
+	for(int i=0; i<2; ++i)
+	{
+		if(i == hightlight)
+			wattron(mainMenu, A_REVERSE);
+		//make sure it's on the center.
+		//12 is length of the "1.Start Game"
+		mvwprintw(mainMenu, i+1, (COLS-16-12)/2, option[i].c_str());
+		wattroff(mainMenu, A_REVERSE);
+	}		
+		choice = wgetch(mainMenu);
+		switch (choice)
+	    	{
+	      	case 10: //10 is the value of the key Enter
+	      		if(hightlight == 0){
+				clear();
+	      			start();  //start the game
+			}
+	      		else if (hightlight == 1){
+				endwin();
+	      			return 0;
+			}
+	      		break;
+		  	case KEY_UP:
+	       	 	--hightlight;
+	       	 	if(hightlight < 0)
+	       	 		hightlight = 0;
+	        	break;
+	      	case KEY_DOWN:
+	       	 	++hightlight;
+	       	 	if(hightlight > 1)
+	       	 		hightlight = 1;
+	        	break;
+	    	default:
+	    		break;
+	    	}
+	    
+	clear();
+	refresh();
+  }
+    endwin(); // ends window functionality
+    return 0;
+   
+}
+
+void start()
+{
+  // TODO if argc == 2, create Hero(viewport, in_
+  
+  Hero * hero; // hero object
+  WINDOW * viewport, * gamemenu; // our 2 windows
   // figures out what the height and width of menu/viewport is
   int gmheight, gmwidth = MENUWIDTH, vpheight, vpwidth;
   vpheight = LINES;
@@ -46,21 +128,9 @@ int main(int argc, char ** argv)
   viewport = newwin(vpheight, vpwidth, 0, 0); // sets location/height/width of viewport
   gamemenu = newwin(gmheight, gmwidth, 0, vpwidth + 1); // same with menu
   wborder(gamemenu, '#', 0, ' ', ' ', '#', 0, '#', 0); // should put just on left side
-
-
-  start_color(); // colors are matched to enum #'s
-  init_pair(1,COLOR_BLACK,COLOR_GREEN);   // MEADOW = 1
-  init_pair(2,COLOR_BLACK,COLOR_MAGENTA); // SWAMP = 2
-  init_pair(3,COLOR_BLACK,COLOR_BLUE);    // WATER = 3
-  init_pair(4,COLOR_BLACK,COLOR_WHITE);   // WALLS = 4
-  init_pair(5,COLOR_BLACK,COLOR_BLACK);   // UNSEEN = 5
-  init_pair(6,COLOR_YELLOW,COLOR_RED);    // Hero
-  init_pair(7,COLOR_WHITE,COLOR_CYAN);    // Royal Diamond
-
-
-  // TODO if argc == 2, create Hero(viewport, in_
-  hero = new Hero(viewport, gamemenu);
  
+  hero = new Hero(viewport, gamemenu);
+  int ch;
   refresh(); // starts the screen
   hero->update_display(); // prints the initial screen
 
@@ -87,7 +157,4 @@ int main(int argc, char ** argv)
     // or option to go again?...
     // also need to make a get_hero_en() function in hero.cpp/h
   }
-
-    endwin(); // ends window functionality
-    return 0;
 }
