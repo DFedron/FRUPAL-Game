2fc7ad6e20f9d350558836729708f9bfb5fc679d
Camilo Schaser-Hughes <camilo3@pdx.edu>
2020-11-30 15:12:57 -0800

    add big castle 80;45ish moved diamond inside

--- map.h
+++ map.h
@@ -7,7 +7,7 @@
 #include <string>
 #include "item_tiles.h"
 #define KSIZE 128 // defines kingdom size
-#define NUMTERRAIN 7 // defines # of terrain files
+#define NUMTERRAIN 8 // defines # of terrain files
 #define NUMITEM 8  //defines # of item filesclass Item; // pre declaration
 
 enum terrain {NONE, MEADOW, SWAMP, WATER, WALL, UNSEEN};
